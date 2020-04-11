75f6ba6e0bc8ae4678af191dce6f24e627652930
thomas-bfs <thomas.boufous@gmail.com>
2020-04-11 14:09:03 +0200

    Reset

--- maze_env.py
+++ maze_env.py
@@ -8,8 +8,8 @@ This script is the environment part of this example. The RL is in RL_brain.py.
 View more on my tutorial page: https://morvanzhou.github.io/tutorials/
 """
 
+
 import numpy as np
-import random
 import time
 import sys
 if sys.version_info.major == 2:
@@ -17,32 +17,18 @@ if sys.version_info.major == 2:
 else:
     import tkinter as tk
 
+
 UNIT = 40   # pixels
 MAZE_H = 8  # grid height
 MAZE_W = 8  # grid width
-origin = np.array([20, 20]) # create origin (center) of the squares
-number_of_hells = 6
-number_of_paradises = 3
 
-# Generate number_of_hells cells randomly
-hell_positions = [(random.randrange(0, 7), random.randrange(0, 7)) for i in range(number_of_hells)]
-hell_coord = []
-paradises_positions = [(random.randrange(0, 7), random.randrange(0, 7)) for i in range(number_of_paradises)]
-paradises_coord = []
-paradises = []
-
-while (0,0) in hell_positions:
-    hell_positions = [(random.randrange(0, 7), random.randrange(0, 7)) for i in range(number_of_hells)]
-for e in hell_positions:
-    while (0,0) in paradises_positions or e in paradises_positions:
-        paradises_positions = [(random.randrange(0, 7), random.randrange(0, 7)) for i in range(number_of_paradises)]
 
 class Maze(tk.Tk, object):
     def __init__(self):
         super(Maze, self).__init__()
         self.action_space = ['u', 'd', 'l', 'r']
         self.n_actions = len(self.action_space)
-        self.title('Garden')
+        self.title('maze')
         self.geometry('{0}x{1}'.format(MAZE_H * UNIT, MAZE_H * UNIT))
         self._build_maze()
 
@@ -50,7 +36,8 @@ class Maze(tk.Tk, object):
         self.canvas = tk.Canvas(self, bg='white',
                            height=MAZE_H * UNIT,
                            width=MAZE_W * UNIT)
-        # Create grids
+
+        # create grids
         for c in range(0, MAZE_W * UNIT, UNIT):
             x0, y0, x1, y1 = c, 0, c, MAZE_H * UNIT
             self.canvas.create_line(x0, y0, x1, y1)
@@ -58,32 +45,36 @@ class Maze(tk.Tk, object):
             x0, y0, x1, y1 = 0, r, MAZE_W * UNIT, r
             self.canvas.create_line(x0, y0, x1, y1)
 
-        # Create hell
-        for i in range(number_of_hells):
-            hell_center = origin + np.array([UNIT * hell_positions[i][0], UNIT*hell_positions[i][1]])
-            self.hell = self.canvas.create_rectangle(
-                hell_center[0] - 15, hell_center[1] - 15,
-                hell_center[0] + 15, hell_center[1] + 15,
-                fill='black')
-            hell_coord.append(self.canvas.coords(self.hell))
+        # create origin
+        origin = np.array([20, 20])
 
-        # Create ovals
-        for i in range(number_of_paradises):
-            oval_center = origin + np.array([UNIT * paradises_positions[i][0], UNIT*paradises_positions[i][1]])
-            self.i = self.canvas.create_oval(
-                oval_center[0] - 15, oval_center[1] - 15,
-                oval_center[0] + 15, oval_center[1] + 15,
-                fill='green')
-            paradises.append(self.canvas.coords(self.i))
-            paradises_coord.append(self.canvas.coords(self.i))
+        # hell number one
+        hell1_center = origin + np.array([UNIT * 2, UNIT])
+        self.hell1 = self.canvas.create_rectangle(
+            hell1_center[0] - 15, hell1_center[1] - 15,
+            hell1_center[0] + 15, hell1_center[1] + 15,
+            fill='black')
+        # hell number 2
+        hell2_center = origin + np.array([UNIT, UNIT * 2])
+        self.hell2 = self.canvas.create_rectangle(
+            hell2_center[0] - 15, hell2_center[1] - 15,
+            hell2_center[0] + 15, hell2_center[1] + 15,
+            fill='black')
 
-        # Create red rect
+        # create oval
+        oval_center = origin + UNIT * 2
+        self.oval = self.canvas.create_oval(
+            oval_center[0] - 15, oval_center[1] - 15,
+            oval_center[0] + 15, oval_center[1] + 15,
+            fill='yellow')
+
+        # create red rect
         self.rect = self.canvas.create_rectangle(
             origin[0] - 15, origin[1] - 15,
             origin[0] + 15, origin[1] + 15,
-            fill='blue')
+            fill='red')
 
-        # Pack all
+        # pack all
         self.canvas.pack()
 
     def reset(self):
@@ -94,8 +85,8 @@ class Maze(tk.Tk, object):
         self.rect = self.canvas.create_rectangle(
             origin[0] - 15, origin[1] - 15,
             origin[0] + 15, origin[1] + 15,
-            fill='blue')
-        # Return observation
+            fill='red')
+        # return observation
         return self.canvas.coords(self.rect)
 
     def step(self, action):
@@ -118,23 +109,25 @@ class Maze(tk.Tk, object):
 
         s_ = self.canvas.coords(self.rect)  # next state
 
-        # Reward function
-        if s_ in paradises_coord:
+        # reward function
+        if s_ == self.canvas.coords(self.oval):
             reward = 1
             done = True
             s_ = 'terminal'
-        elif s_ in hell_coord:
+        elif s_ in [self.canvas.coords(self.hell1), self.canvas.coords(self.hell2)]:
             reward = -1
             done = True
             s_ = 'terminal'
-        else: # if normal white empty cell
+        else:
             reward = 0
             done = False
+
         return s_, reward, done
 
     def render(self):
         time.sleep(0.1)
         self.update()
+
 
 def update():
     for t in range(10):
